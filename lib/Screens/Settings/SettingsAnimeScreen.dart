c5ee6f02ef3dc29c3117ae91eed310a047cb1787
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2025-03-01 19:03:39 +0530

    Update SettingsAnimeScreen.dart

--- lib/Screens/Settings/SettingsAnimeScreen.dart
+++ lib/Screens/Settings/SettingsAnimeScreen.dart
@@ -112,7 +112,7 @@ class SettingsAnimeScreenState extends BaseSettingsScreen {
                   List<bool>.from(homeLayoutMap.values.toList());
               AlertDialogBuilder(context)
                 ..setTitle(
-                    getString.manageLayout(getString.home, getString.mal))
+                    getString.manageLayout(getString.mal, getString.home))
                 ..reorderableMultiSelectableItems(
                   titles,
                   checkedStates,
@@ -144,7 +144,7 @@ class SettingsAnimeScreenState extends BaseSettingsScreen {
         settings: [
           Setting(
             type: SettingType.normal,
-            name: getString.manageLayout(getString.home, getString.simkl),
+            name: getString.manageLayout(getString.simkl, getString.home),
             description: getString.manageLayoutDescription(getString.home),
             icon: Icons.tune,
             onClick: () async {
@@ -154,7 +154,7 @@ class SettingsAnimeScreenState extends BaseSettingsScreen {
               List<bool>.from(homeLayoutMap.values.toList());
               AlertDialogBuilder(context)
                 ..setTitle(
-                    getString.manageLayout(getString.simkl, getString.home))
+                    getString.manageLayout(getString.home, getString.simkl))
                 ..reorderableMultiSelectableItems(
                   titles,
                   checkedStates,
